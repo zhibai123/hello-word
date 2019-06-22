b24f3fd6402393e9f95dd13eb7e7de7cbc28768c
Your Name <email@example.com>
2019-06-22 17:49:35 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "hellodialog.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QDialog w;
+    Ui::HelloDialog ui;
+    ui.setupUi(&w);
+    w.show();
+    return a.exec();
+}
+
